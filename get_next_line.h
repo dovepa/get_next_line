8ff56194aa76ae2f682b820595e28552e35c6977
Unknow <unknow@Unknows-Mac.local>
2018-03-15 10:06:10 +0100

    a

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpalombo <dpalombo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/18 12:09:33 by dpalombo          #+#    #+#             */
+/*   Updated: 2018/02/06 13:49:51 by dpalombo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# define BUFF_SIZE 42
+
+int	get_next_line(const int fd, char **line);
+
+#endif
